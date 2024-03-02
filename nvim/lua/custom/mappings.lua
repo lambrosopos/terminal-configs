bcc68c4d67c950d7c2e0bda0ce9134719eb86a30
lambrosopos <lambrosopos@gmail.com>
2024-03-03 00:01:46 +0900

    update: add nvchad custom configs

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,34 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    },
+
+    -- change to current file directory
+    -- ["<leader>cd"] = { ":cd %:p:h <CR>:pwd <CR>", "Change to Buffer Dir"},
+    -- ["<leader>cd"] = { "<cmd> lcd %:p:h <CR>", "Change to Buffer Dir"},
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+M.vim_fugitive = {
+  n = {
+    ["<leader>go"] = { "<cmd> tab Git <CR>", "Git Overview" },
+    ["<leader>gc"] = { "<cmd> Git commit <CR>", "Git Commit"},
+  }
+}
+
+return M
